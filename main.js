5c5b8849aa177c0d03ba245a1878630bd14fdfaa
FazliAlhajj <111104321+FazliAlhajj@users.noreply.github.com>
2023-01-31 21:36:00 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// BOOTSTRAP
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import "@popperjs/core"
+
+// PROGRESS BAR
+import VueProgressBar from "@aacassandra/vue3-progressbar";
+
+const options = {
+    color: "#f00",
+    failedColor: "#874b4b",
+    thickness: "5px",
+    transition: {
+        speed: "0.2s",
+        opacity: "0.6s",
+        termination: 300,
+    },
+    autoRevert: true,
+    location: "top",
+    inverse: false,
+};
+
+// Vue Writer 
+import VueWriter from "vue-writer";
+
+createApp(App).use(VueWriter).use(VueProgressBar, options).use(router).mount('#app')
